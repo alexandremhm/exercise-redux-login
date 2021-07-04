fb97331673f266b157dc71243a447fc6ff687b2e
Marco Galindo <marco1408@gmail.com>
2021-07-04 11:59:39 -0300

    requisito obrigatorio finalizado

--- src/pages/Clients.jsx
+++ src/pages/Clients.jsx
@@ -2,6 +2,15 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 class Clients extends Component {
+  constructor() {
+    super();
+    this.removeClient = this.removeClient.bind(this);
+  }
+
+  removeClient({target}) {
+    console.log(target.parentNode.key);
+  }
+
   render() {
     const { login, clients } = this.props;
     return (
@@ -9,7 +18,10 @@ class Clients extends Component {
         { login === '' && <Redirect to="/loggedout" />}
         <p>{login}</p>
         { clients.length === 0 && <p>Nenhum Cliente Cadastrado</p> }
-        { clients !== undefined && clients.map((client) => <div>{ `${client.name} ${client.email} ${client.age}`}</div>)}
+        { clients !== undefined && clients.map((client, index) => 
+          <div key={index}>{ `${client.name} ${client.email} ${client.age}`}
+          <button onClick={this.removeClient}>X</button>
+          </div>)}
         <Link to="/register">Register</Link>  
       </div>
     )
